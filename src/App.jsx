68fb1f7f601a1acbb7027138097650597347d153
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-02-07 16:06:33 +0530

    router configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-	return <>Home page</>;
+	return (
+		<>
+			<Outlet />
+		</>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+	About,
+	Contact,
+	Courses,
+	Error,
+	Faq,
+	RecipeIndex,
+} from './pages/Pages.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/about',
+				element: <About />,
+				errorElement: <Error />,
+			},
+			{
+				path: '/contact',
+				element: <Contact />,
+				errorElement: <Error />,
+			},
+			{
+				path: '/courses',
+				element: <Courses />,
+				errorElement: <Error />,
+			},
+			{
+				path: '/faq',
+				element: <Faq />,
+				errorElement: <Error />,
+			},
+			{
+				path: '/recipe',
+				element: <RecipeIndex />,
+				errorElement: <Error />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
