c05308a0a84a31a2a05c63d5719d72330bb79c7c
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-02-07 16:06:02 +0530

    pages created for router

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,8 @@
+import About from './About';
+import Contact from './Contact';
+import Courses from './Courses';
+import Error from './Error';
+import Faq from './Faq';
+import RecipeIndex from './RecipeIndex';
+
+export { About, Contact, Courses, Error, Faq, RecipeIndex };
