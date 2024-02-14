e7eae730efaef691458f791ae56403af081905ca
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-02-14 19:08:58 +0530

    Nav bar Fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
 	Navbar,
-	MobileNav,
+	Collapse,
 	Typography,
 	Button,
 	IconButton,
 } from '@material-tailwind/react';
 
 export function Header() {
-	const [openNav, setOpenNav] = useState(false);
+	const [openNav, setOpenNav] = React.useState(false);
 
-	useEffect(() => {
+	React.useEffect(() => {
 		window.addEventListener(
 			'resize',
-			() => window.innerWidth >= 960 && setOpenNav(false),
+			() => window.innerWidth >= 720 && setOpenNav(false),
 		);
 	}, []);
 
 	const navList1 = (
-		<ul className='mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
+		<ul className='mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 '>
 			<Typography
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/'
 					className='flex items-center'>
@@ -34,7 +34,7 @@ export function Header() {
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/courses'
 					className='flex items-center'>
@@ -45,22 +45,23 @@ export function Header() {
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/recipe'
 					className='flex items-center'>
-					Recipe
+					Recipes
 				</a>
 			</Typography>
 		</ul>
 	);
+
 	const navList2 = (
-		<ul className='mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
+		<ul className='mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row  lg:items-center lg:gap-6'>
 			<Typography
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/about'
 					className='flex items-center'>
@@ -71,7 +72,7 @@ export function Header() {
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/contact'
 					className='flex items-center'>
@@ -82,7 +83,7 @@ export function Header() {
 				as='li'
 				variant='small'
 				color='blue-gray'
-				className='p-1 font-normal'>
+				className='p-1 font-semibold text-blue-gray-400'>
 				<a
 					href='/faq'
 					className='flex items-center'>
@@ -93,87 +94,92 @@ export function Header() {
 	);
 
 	return (
-		<div className=' '>
+		<div className=' w-[calc(100%)] max-h-[768px] fixed z-50'>
 			<Navbar className='sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4'>
 				<div className='flex items-center justify-between text-blue-gray-900'>
-					<div className='flex items-center gap-4'>
-						<div className='mr-4 hidden lg:block'>{navList1}</div>
-						<Typography
-							as='a'
-							href='/'
-							className='mr-4 cursor-pointer py-1.5 font-medium'>
-							Material Tailwind
-						</Typography>
-						<div className='mr-4 hidden lg:block'>{navList2}</div>
-						<div className='flex items-center gap-x-1'>
-							<Button
-								variant='text'
-								size='sm'
-								className='hidden lg:inline-block'>
-								<span>Log In</span>
-							</Button>
-							<Button
-								variant='gradient'
-								size='sm'
-								className='hidden lg:inline-block'>
-								<span>Sign in</span>
-							</Button>
-						</div>
-						<IconButton
+					<div className='mr-10 hidden lg:block'>{navList1}</div>
+					<Typography
+						as='a'
+						href='/'
+						className='mx-20 font-bold cursor-pointer'>
+						<img
+							alt='logo'
+							className=' w-[10rem] '
+							src='public/logo.svg'
+						/>
+					</Typography>
+					<div className='mr-4 hidden lg:block'>{navList2}</div>
+					<div className='flex items-center gap-x-1 '>
+						<Button
 							variant='text'
-							className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
-							ripple={false}
-							onClick={() => setOpenNav(!openNav)}>
-							{openNav ? (
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									className='h-6 w-6'
-									viewBox='0 0 24 24'
-									stroke='currentColor'
-									strokeWidth={2}>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										d='M6 18L18 6M6 6l12 12'
-									/>
-								</svg>
-							) : (
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-6 w-6'
-									fill='none'
-									stroke='currentColor'
-									strokeWidth={2}>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										d='M4 6h16M4 12h16M4 18h16'
-									/>
-								</svg>
-							)}
-						</IconButton>
+							size='sm'
+							className='hidden lg:inline-block'>
+							<span>Log In</span>
+						</Button>
+						<Button
+							variant='gradient'
+							size='sm'
+							className='hidden lg:inline-block'>
+							<span>Sign in</span>
+						</Button>
 					</div>
+					<IconButton
+						variant='text'
+						className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
+						ripple={false}
+						onClick={() => setOpenNav(!openNav)}>
+						{openNav ? (
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								className='h-6 w-6'
+								viewBox='0 0 24 24'
+								stroke='currentColor'
+								strokeWidth={2}>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M6 18L18 6M6 6l12 12'
+								/>
+							</svg>
+						) : (
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='h-6 w-6'
+								fill='none'
+								stroke='currentColor'
+								strokeWidth={2}>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M4 6h16M4 12h16M4 18h16'
+								/>
+							</svg>
+						)}
+					</IconButton>
 				</div>
-				<MobileNav open={openNav}>
-					{navList1}
+				<Collapse open={openNav}>
+					<div className='flex items-center gap-x-6 ml-8'>
+						<div className='flex items-center'>{navList1}</div>
+						<div className='flex items-center'>{navList2}</div>
+					</div>
 					<div className='flex items-center gap-x-1'>
 						<Button
 							fullWidth
 							variant='text'
 							size='sm'
 							className=''>
-							<span>Log In</span>
+							<span className='text-blue-gray-600'>Log In</span>
 						</Button>
 						<Button
 							fullWidth
 							variant='gradient'
 							size='sm'
 							className=''>
-							<span>Sign in</span>
+							<span className='text-blue-gray-200'>Sign in</span>
 						</Button>
 					</div>
-				</MobileNav>
+				</Collapse>
 			</Navbar>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = withMT({
 			'text-base': 'white',
 		},
 		extend: {},
+		screens: {
+			sm: '540px',
+			md: '720px',
+			'md-max': { max: '720px' },
+			lg: '960px',
+			xl: '1140px',
+			'2xl': '1320px',
+		},
 	},
 	plugins: [],
 });
