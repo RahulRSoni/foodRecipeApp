ef5c9a8123b3923bf2e3a2087e4a13dac7975ab9
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-04-15 18:43:42 +0530

    Try to fixing Form Issue

---
+++ semple.js
@@ -0,0 +1,356 @@
+import {
+	Input,
+	Button,
+	Typography,
+	IconButton,
+	Select,
+	Tooltip,
+	Textarea,
+	Option,
+} from '@material-tailwind/react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { CommentBox } from '../RecipeFrom/CommentBox';
+import Ingredient from '../RecipeFrom/Ingredients';
+
+const PostForm = ({ post }) => {
+	const navigate = useNavigate();
+
+	const handlePostsChange = (updatedPosts) => {
+		// Do something with the updated posts data
+		console.log('Updated posts:', updatedPosts);
+	};
+
+	// const userData = useSelector((state) => state.userData);
+	const { register, handleSubmit, watch, setValue, control, getValues } =
+		useForm({
+			defaultValues: {
+				title: post?.title || '',
+				slug: post?.slug || '',
+				content: post?.content || '',
+				status: post?.status || 'active',
+				featuredImages: post?.featuredImages || [],
+				keyword: post?.keyword || '',
+				cuisine: post?.cuisine || '',
+				course: post?.course || '',
+				calories: post?.calories || '',
+				serving: post?.serving || '',
+				preparingTime: post?.preparingTime || '',
+				cockingTime: post?.cockingTime || '',
+				restingTime: post?.restingTime || '',
+				totalTime: post?.totalTime || '',
+				bakingTime: post?.bakingTime || '',
+				featuredImages2: post?.featuredImages2,
+			},
+		});
+
+	const submit = async (data) => {
+		try {
+			// Asynchronous operation
+			const formData = {
+				...data,
+			};
+			console.log(formData);
+		} catch (error) {
+			console.error('Error occurred:', error);
+		}
+	};
+	const slugTransform = useCallback((value) => {
+		if (value && typeof value === 'string')
+			return value.trim().toLowerCase().replace(/\s/g, '-');
+
+		return '';
+	}, []);
+
+	useEffect(() => {
+		const subscription = watch((value, { name }) => {
+			if (name === 'title') {
+				setValue('slug', slugTransform(value.title), { shouldValidate: true });
+			}
+		});
+		return () => {
+			subscription.unsubscribe();
+		};
+	}, [watch, slugTransform, setValue]);
+
+	return (
+		<form onSubmit={handleSubmit(submit)}>
+			<div className='flex flex-col justify-center items-center p-10 mt-8 '>
+				<div className=' mb-4 flex flex-col justify-center gap-5 bg-blue-gray-50 backdrop-blur-sm py-8 rounded-lg px-6'>
+					<Input
+						label='Title :'
+						placeholder='Title'
+						size='md'
+						name='title'
+						{...register('title')}
+					/>
+					<Input
+						label='Slug :'
+						placeholder='Slug'
+						size='md'
+						name='slug'
+						{...register('slug')}
+						onInput={(e) => {
+							setValue('slug', slugTransform(e.target.value), {
+								shouldValidate: true,
+							});
+						}}
+					/>
+					<div className='relative flex gap-2 w-full'>
+						<Input
+							label='Featured Image :'
+							size='md'
+							type='file'
+							name='featuredImages'
+							accept='image/png, image/jpg, image/jpeg, image/gif'
+							{...register('featuredImages')}
+						/>
+
+						<Tooltip
+							placement='top'
+							className='border border-blue-gray-50 bg-white shadow-xl shadow-black/10'
+							content={
+								<Typography
+									color='blue-gray'
+									className='font-medium font-serif'>
+									Upload
+								</Typography>
+							}>
+							<IconButton
+								variant='text'
+								className='!absolute right-1 top-1 rounded w-8 h-8'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18'
+									/>
+								</svg>
+							</IconButton>
+						</Tooltip>
+						{post && (
+							<div className='w-full mb-4 flex item-center border-r-2 border-blue-gray-100'>
+								<img
+									src={post.getFilePreview(post.featuredImage)}
+									alt={post.title}
+									className='rounded-lg'
+								/>
+							</div>
+						)}
+					</div>
+					<Textarea
+						label='Content'
+						name='content'
+						{...register('content')}
+					/>
+					<Controller
+						name='select'
+						control={control}
+						defaultValue='active'
+						render={({ field }) => (
+							<Select
+								label='Status'
+								value='active'
+								{...field}>
+								<Option value='active'>Active</Option>
+								<Option value='Inactive'>Inactive </Option>
+							</Select>
+						)}
+					/>
+
+					<Typography
+						as='h3'
+						className='font-semibold'>
+						Recipe Basic Details
+					</Typography>
+
+					<div className='flex flex-wrap gap-2 w-full'>
+						<div>
+							<Input
+								label='Serving :'
+								size='md'
+								placeholder='How many people for serving.'
+								name='Serving'
+								{...register('serving')}
+							/>
+						</div>
+						<div>
+							<Input
+								label='Preparing Time :'
+								size='md'
+								placeholder='How much time to prepare.'
+								name='preparingTime'
+								{...register('preparingTime')}
+							/>
+						</div>
+						<div>
+							<Input
+								label='Coke Time :'
+								size='md'
+								placeholder='How much time to cocking.'
+								name='cockingTime'
+								{...register('cockingTime')}
+							/>
+						</div>
+					</div>
+					<div className='flex flex-wrap gap-2 w-full'>
+						<div>
+							<Input
+								label='Resting Time :'
+								size='md'
+								placeholder='How much time to resting.'
+								name='restingTime'
+								{...register('restingTime')}
+							/>
+						</div>
+						<div>
+							<Input
+								label='Total time :'
+								size='md'
+								placeholder='Total time for preparing.'
+								name='totalTime'
+								{...register('totalTime')}
+							/>
+						</div>
+						<div>
+							<Input
+								label='Baking time :'
+								size='md'
+								placeholder='Total time for preparing.'
+								name='bakingTime'
+								{...register('bakingTime')}
+							/>
+						</div>
+					</div>
+
+					<div className='relative flex gap-2 w-full'>
+						<Input
+							label='Featured Image 2:'
+							size='md'
+							type='file'
+							accept='image/png, image/jpg, image/jpeg, image/gif'
+							{...register('featuredImages2')}
+						/>
+
+						<Tooltip
+							placement='top'
+							className='border border-blue-gray-50 bg-white shadow-xl shadow-black/10'
+							content={
+								<Typography
+									color='blue-gray'
+									className='font-medium font-serif'>
+									Upload
+								</Typography>
+							}>
+							<IconButton
+								variant='text'
+								className='!absolute right-1 top-1 rounded w-8 h-8'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18'
+									/>
+								</svg>
+							</IconButton>
+						</Tooltip>
+						{post && (
+							<div className='w-full mb-4 flex item-center border-r-2 border-blue-gray-100'>
+								<img
+									src={post.getFilePreview(post.featuredImage)}
+									alt={post.title}
+									className='rounded-lg'
+								/>
+							</div>
+						)}
+					</div>
+					<Typography
+						as='h3'
+						className='font-semibold'>
+						Recipe Contain:
+					</Typography>
+					<div className='flex justify-between w-full gap-2'>
+						<div className='flex flex-col w-full gap-2 '>
+							<Input
+								label='Keyword :'
+								size='md'
+								name='keyword'
+								className='w-full'
+								{...register('keyword')}
+							/>
+							<Input
+								label='Cuisine :'
+								size='md'
+								name='cuisine'
+								className='w-full'
+								{...register('cuisine')}
+							/>
+						</div>
+						<div className='flex flex-col w-full  gap-2 '>
+							<Input
+								label='Course :'
+								size='md'
+								name='course'
+								className='w-full'
+								{...register('course')}
+							/>
+							<Input
+								label='Calories :'
+								size='md'
+								name='calories'
+								className='w-full'
+								{...register('calories')}
+							/>
+						</div>
+					</div>
+					<Typography
+						as='h3'
+						className='font-semibold'>
+						Recipe Ingredients:
+					</Typography>
+					<div className='flex flex-col gap-2 w-full'>
+						<Ingredient
+							control={control}
+							register={register}
+							setValue={setValue}
+						/>
+					</div>
+					<Typography
+						as='h3'
+						className='font-semibold'>
+						Recipe Steps:
+					</Typography>
+					<CommentBox
+						control={control}
+						register={register}
+					/>
+					<div className='w-full'>
+						<div className='flex gap-2 justify-end'>
+							<Button
+								size='sm'
+								type='submit'
+								className=' bg-green-500'>
+								{post ? 'Update' : 'Submit'}
+							</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default PostForm;
--- src/components/RecipeFrom/CommentBox.jsx
+++ src/components/RecipeFrom/CommentBox.jsx
@@ -7,8 +7,11 @@ import {
 	Button,
 } from '@material-tailwind/react';
 
-export function CommentBox({ control, register, onPostsChange }) {
-	const [posts, setPosts] = useState([{ text: '', image: null }]);
+export function CommentBox({ control, register }) {
+	const [posts, setPosts] = useState([
+		{ text: '', image: null },
+		{ text: '', image: null },
+	]);
 	const [stepCount, setStepCount] = useState(1);
 
 	useEffect(() => {
@@ -60,7 +63,6 @@ export function CommentBox({ control, register, onPostsChange }) {
 			newPosts[index] = { ...newPosts[index], text: newText };
 			return newPosts;
 		});
-		onPostsChange(newPosts); // Pass the updated posts array to the parent component
 	};
 
 	return (
@@ -96,19 +98,7 @@ export function CommentBox({ control, register, onPostsChange }) {
 								size='sm'
 								onClick={() => handleAddImage(index)}
 								disabled={post.image !== null}>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									strokeWidth={1.5}
-									stroke='currentColor'
-									className='w-6 h-6'>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										d='m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
-									/>
-								</svg>
+								{/* Add your SVG icon here */}
 							</IconButton>
 						</Tooltip>
 						{/* Button to delete post */}
@@ -128,19 +118,7 @@ export function CommentBox({ control, register, onPostsChange }) {
 								size='sm'
 								onClick={() => handleDeletePost(index)}
 								disabled={posts.length === 1}>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									strokeWidth={1.5}
-									stroke='currentColor'
-									className='w-6 h-6'>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
-									/>
-								</svg>
+								{/* Add your SVG icon here */}
 							</IconButton>
 						</Tooltip>
 					</div>
@@ -171,7 +149,6 @@ export function CommentBox({ control, register, onPostsChange }) {
 					color='green'
 					size='sm'
 					className='float-right'>
-					{' '}
 					Add More
 				</Button>
 			</Tooltip>
