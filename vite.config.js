927c5fb5bef8a2a2dc90a479615ca5dca2852360
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2024-05-15 22:21:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		[
-			'@babel/plugin-transform-react-jsx',
-			{
-				throwIfNamespace: false,
-			},
-		],
-	],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
 });
